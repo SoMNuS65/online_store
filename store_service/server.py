e1c6bf27badf12f8f8b4de3de78456001bea9418
SoMNuS65 <121434983+SoMNuS65@users.noreply.github.com>
2024-06-24 12:11:17 +0500

    Delete store_service directory

--- store_service/server.py
+++
@@ -1,9 +0,0 @@
-from store_app import app, db
-from store_app.models import Category, Good
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'app' : app, 'db' : db, 'Category' : Category, "Good" : Good}
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True)
