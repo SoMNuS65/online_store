d136b744ac6d0d4e2bed5cd76de1856a1c8f7573
SoMNuS65 <121434983+SoMNuS65@users.noreply.github.com>
2024-06-24 12:11:07 +0500

    Delete auth_service directory

--- auth_service/auth_app/validators.py
+++
@@ -1,4 +0,0 @@
-from marshmallow import Schema, fields, validate
-
-class LoginBodyValidation(Schema):
-    email = fields.String(required=True, validate=[validate.Email(), validate.Length(min=1, max=60)])
--- auth_service/server.py
+++
@@ -1,9 +0,0 @@
-from auth_app import app, db
-from auth_app.models import Validation, User
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'app' : app, 'db' : db, 'User' : User, 'Validation' : Validation}
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True)
